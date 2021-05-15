6e1a13babd16667b1bc3b1f4f2bc0f2f7f1ce0a1
Jacques <jacques.tellier@hotmail.com>
2021-05-15 15:13:18 +0200

    corrections pour cohérence 2

--- sql.sql
+++ sql.sql
@@ -71,14 +71,14 @@ CREATE TABLE Client(
 #------------------------------------------------------------
 
 CREATE TABLE ProgrammeFidelite(
-        refProgrammeFidelite Int  Auto_increment  NOT NULL ,
-        duree                Int NOT NULL ,
-        description          Text NOT NULL ,
-        prix                 Float NOT NULL ,
-        tauxReduction        Float NOT NULL ,
-        dateSoucription      Date NOT NULL ,
-        idUtilisateur        Int NOT NULL
-	,CONSTRAINT ProgrammeFidelite_PK PRIMARY KEY (refProgrammeFidelite)
+        idProgrammeFidelite Int  Auto_increment  NOT NULL ,
+        duree               Int NOT NULL ,
+        description         Text NOT NULL ,
+        prix                Float NOT NULL ,
+        tauxReduction       Float NOT NULL ,
+        dateSoucription     Date NOT NULL ,
+        idUtilisateur       Int NOT NULL
+	,CONSTRAINT ProgrammeFidelite_PK PRIMARY KEY (idProgrammeFidelite)
 
 	,CONSTRAINT ProgrammeFidelite_Client_FK FOREIGN KEY (idUtilisateur) REFERENCES Client(idUtilisateur)
 )ENGINE=InnoDB;
@@ -89,13 +89,13 @@ CREATE TABLE ProgrammeFidelite(
 #------------------------------------------------------------
 
 CREATE TABLE Agence(
-        identifiantAgence  Int  Auto_increment  NOT NULL ,
+        idAgence           Int  Auto_increment  NOT NULL ,
         nom                Varchar (50) NOT NULL ,
         telephone          Char (10) NOT NULL ,
         adresse            Varchar (50) NOT NULL ,
         coordonneesGPS     Varchar (50) NOT NULL ,
         nombreMaxVehicules Int NOT NULL
-	,CONSTRAINT Agence_PK PRIMARY KEY (identifiantAgence)
+	,CONSTRAINT Agence_PK PRIMARY KEY (idAgence)
 )ENGINE=InnoDB;
 
 
@@ -104,19 +104,19 @@ CREATE TABLE Agence(
 #------------------------------------------------------------
 
 CREATE TABLE Employe(
-        idUtilisateur     Int NOT NULL ,
-        login             Varchar (50) NOT NULL ,
-        motDePasse        Varchar (50) NOT NULL ,
-        nom               Varchar (50) NOT NULL ,
-        prenom            Varchar (50) NOT NULL ,
-        email             Varchar (50) NOT NULL ,
-        adresse           Varchar (50) NOT NULL ,
-        telephone         Char (10) NOT NULL ,
-        identifiantAgence Int NOT NULL
+        idUtilisateur Int NOT NULL ,
+        login         Varchar (50) NOT NULL ,
+        motDePasse    Varchar (50) NOT NULL ,
+        nom           Varchar (50) NOT NULL ,
+        prenom        Varchar (50) NOT NULL ,
+        email         Varchar (50) NOT NULL ,
+        adresse       Varchar (50) NOT NULL ,
+        telephone     Char (10) NOT NULL ,
+        idAgence      Int NOT NULL
 	,CONSTRAINT Employe_PK PRIMARY KEY (idUtilisateur)
 
 	,CONSTRAINT Employe_Utilisateur_FK FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur(idUtilisateur)
-	,CONSTRAINT Employe_Agence0_FK FOREIGN KEY (identifiantAgence) REFERENCES Agence(identifiantAgence)
+	,CONSTRAINT Employe_Agence0_FK FOREIGN KEY (idAgence) REFERENCES Agence(idAgence)
 )ENGINE=InnoDB;
 
 
@@ -125,19 +125,19 @@ CREATE TABLE Employe(
 #------------------------------------------------------------
 
 CREATE TABLE Chauffeur(
-        idUtilisateur     Int NOT NULL ,
-        nom               Varchar (50) NOT NULL ,
-        prenom            Varchar (50) NOT NULL ,
-        email             Varchar (50) NOT NULL ,
-        adresse           Varchar (50) NOT NULL ,
-        telephone         Char (10) NOT NULL ,
-        login             Varchar (50) NOT NULL ,
-        motDePasse        Varchar (50) NOT NULL ,
-        identifiantAgence Int NOT NULL
+        idUtilisateur Int NOT NULL ,
+        nom           Varchar (50) NOT NULL ,
+        prenom        Varchar (50) NOT NULL ,
+        email         Varchar (50) NOT NULL ,
+        adresse       Varchar (50) NOT NULL ,
+        telephone     Char (10) NOT NULL ,
+        login         Varchar (50) NOT NULL ,
+        motDePasse    Varchar (50) NOT NULL ,
+        idAgence      Int NOT NULL
 	,CONSTRAINT Chauffeur_PK PRIMARY KEY (idUtilisateur)
 
 	,CONSTRAINT Chauffeur_Employe_FK FOREIGN KEY (idUtilisateur) REFERENCES Employe(idUtilisateur)
-	,CONSTRAINT Chauffeur_Agence0_FK FOREIGN KEY (identifiantAgence) REFERENCES Agence(identifiantAgence)
+	,CONSTRAINT Chauffeur_Agence0_FK FOREIGN KEY (idAgence) REFERENCES Agence(idAgence)
 )ENGINE=InnoDB;
 
 
@@ -146,12 +146,12 @@ CREATE TABLE Chauffeur(
 #------------------------------------------------------------
 
 CREATE TABLE Location(
-        refLocation   Int  Auto_increment  NOT NULL ,
+        idLocation    Int  Auto_increment  NOT NULL ,
         duree         Time NOT NULL ,
         assurance     Bool NOT NULL ,
         idUtilisateur Int NOT NULL ,
         matricule     Varchar (7) NOT NULL
-	,CONSTRAINT Location_PK PRIMARY KEY (refLocation)
+	,CONSTRAINT Location_PK PRIMARY KEY (idLocation)
 
 	,CONSTRAINT Location_Client_FK FOREIGN KEY (idUtilisateur) REFERENCES Client(idUtilisateur)
 	,CONSTRAINT Location_Vehicule0_FK FOREIGN KEY (matricule) REFERENCES Vehicule(matricule)
@@ -167,11 +167,11 @@ CREATE TABLE Facture(
         dureeEffective       Time NOT NULL ,
         consomationCarburant Float NOT NULL ,
         etatVehicule         Varchar (50) NOT NULL ,
-        identifiantAgence    Int ,
+        idAgence             Int ,
         idUtilisateur        Int NOT NULL
 	,CONSTRAINT Facture_PK PRIMARY KEY (idFacture)
 
-	,CONSTRAINT Facture_Agence_FK FOREIGN KEY (identifiantAgence) REFERENCES Agence(identifiantAgence)
+	,CONSTRAINT Facture_Agence_FK FOREIGN KEY (idAgence) REFERENCES Agence(idAgence)
 	,CONSTRAINT Facture_Client0_FK FOREIGN KEY (idUtilisateur) REFERENCES Client(idUtilisateur)
 )ENGINE=InnoDB;
 
@@ -181,19 +181,19 @@ CREATE TABLE Facture(
 #------------------------------------------------------------
 
 CREATE TABLE Gerant(
-        idUtilisateur     Int NOT NULL ,
-        nom               Varchar (50) NOT NULL ,
-        prenom            Varchar (50) NOT NULL ,
-        email             Varchar (50) NOT NULL ,
-        adresse           Varchar (50) NOT NULL ,
-        telephone         Char (10) NOT NULL ,
-        login             Varchar (50) NOT NULL ,
-        motDePasse        Varchar (50) NOT NULL ,
-        identifiantAgence Int NOT NULL
+        idUtilisateur Int NOT NULL ,
+        nom           Varchar (50) NOT NULL ,
+        prenom        Varchar (50) NOT NULL ,
+        email         Varchar (50) NOT NULL ,
+        adresse       Varchar (50) NOT NULL ,
+        telephone     Char (10) NOT NULL ,
+        login         Varchar (50) NOT NULL ,
+        motDePasse    Varchar (50) NOT NULL ,
+        idAgence      Int NOT NULL
 	,CONSTRAINT Gerant_PK PRIMARY KEY (idUtilisateur)
 
 	,CONSTRAINT Gerant_Employe_FK FOREIGN KEY (idUtilisateur) REFERENCES Employe(idUtilisateur)
-	,CONSTRAINT Gerant_Agence0_FK FOREIGN KEY (identifiantAgence) REFERENCES Agence(identifiantAgence)
+	,CONSTRAINT Gerant_Agence0_FK FOREIGN KEY (idAgence) REFERENCES Agence(idAgence)
 )ENGINE=InnoDB;
 
 
@@ -205,11 +205,11 @@ CREATE TABLE Devis(
         idDevis             Int  Auto_increment  NOT NULL ,
         montantReduction    Float NOT NULL ,
         dureePrevueLocation Int NOT NULL ,
-        identifiantAgence   Int ,
+        idAgence            Int ,
         idUtilisateur       Int
 	,CONSTRAINT Devis_PK PRIMARY KEY (idDevis)
 
-	,CONSTRAINT Devis_Agence_FK FOREIGN KEY (identifiantAgence) REFERENCES Agence(identifiantAgence)
+	,CONSTRAINT Devis_Agence_FK FOREIGN KEY (idAgence) REFERENCES Agence(idAgence)
 	,CONSTRAINT Devis_Client0_FK FOREIGN KEY (idUtilisateur) REFERENCES Client(idUtilisateur)
 )ENGINE=InnoDB;
 
