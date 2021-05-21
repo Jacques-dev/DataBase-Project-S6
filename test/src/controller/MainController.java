d52734328f6b99e954a03680aa23eb7b51aba440
Merouane <merouane.souadji@efrei.net>
2021-05-21 14:12:31 +0200

    scenebuilder_test

--- test/src/controller/MainController.java
+++ test/src/controller/MainController.java
@@ -1,5 +1,19 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainController {
+	
+	
+	@FXML
+	private Button btn;
+	
+	public void exit(ActionEvent event) {
+		System.out.println("salut");
+		System.exit(0);
+	}
+	
 	
 }
--- test/src/module-info.java
+++ test/src/module-info.java
@@ -1,6 +1,7 @@
 module test {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens controller to javafx.graphics, javafx.fxml;
 }
