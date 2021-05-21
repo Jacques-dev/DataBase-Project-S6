44476ef1ee44615816eae82ddfd0885897f355fe
Merouane <merouane.souadji@efrei.net>
2021-05-21 13:50:58 +0200

    test encore

--- test/src/module-info.java
+++ test/src/module-info.java
@@ -1,8 +1,6 @@
-module Project {
+module test {
 	requires javafx.controls;
 	requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
 	
 	opens controller to javafx.graphics, javafx.fxml;
 }
