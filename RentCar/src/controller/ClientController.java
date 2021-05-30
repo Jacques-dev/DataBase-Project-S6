ae988a59e115a683af9b4130e123e520dc2f05f1
Jacques <jacques.tellier@hotmail.com>
2021-05-30 19:49:37 +0200

    Client casi fini mon reuf

--- RentCar/src/controller/ClientController.java
+++ RentCar/src/controller/ClientController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,10 +15,13 @@ import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -26,7 +32,7 @@ import model.ProgramFidelite;
 import model.Utilisateur;
 import model.Vehicule;
 
-public class ClientController extends MainController {
+public class ClientController extends MainController implements Initializable {
 
 	@FXML private TableView<ProgramFidelite> programFideliteTable;
 	@FXML private TableColumn<ProgramFidelite, Integer> idProgrammeFidelite;
@@ -52,6 +58,7 @@ public class ClientController extends MainController {
         prix.setCellValueFactory(new PropertyValueFactory<ProgramFidelite, Float>("prix"));
 		
         programFideliteTable.setItems(programs);
+        print_clients(event);
 	}
 	
 	@FXML private TableView<Vehicule> vehiculeTable;
@@ -68,6 +75,27 @@ public class ClientController extends MainController {
 	
 	public void printVehicules(ActionEvent event) throws SQLException {
         String sql = "Select * From vehicule WHERE matricule not in (select matricule from loue)";
+        PreparedStatement stat = conn.prepareStatement(sql);
+        ResultSet rs = stat.executeQuery();
+        
+        while(rs.next()) {
+        	vehicules.add(new Vehicule(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getBoolean(5), rs.getString(6), rs.getString(7)));
+        }
+        
+        matricule.setCellValueFactory(new PropertyValueFactory<Vehicule, String>("matricule"));
+        marque.setCellValueFactory(new PropertyValueFactory<Vehicule, String>("marque"));
+        modele.setCellValueFactory(new PropertyValueFactory<Vehicule, String>("modele"));
+        kilometrage.setCellValueFactory(new PropertyValueFactory<Vehicule, Integer>("kilometrage"));
+        climatisation.setCellValueFactory(new PropertyValueFactory<Vehicule, Boolean>("climatisation"));
+        typeBoiteDeVitesse.setCellValueFactory(new PropertyValueFactory<Vehicule, String>("typeBoiteDeVitesse"));
+        type.setCellValueFactory(new PropertyValueFactory<Vehicule, String>("type"));
+		
+        vehiculeTable.setItems(vehicules);
+        print_clients(event);
+	}
+	
+	public void printVehiculesLoue(ActionEvent event) throws SQLException {
+        String sql = "Select * From vehicule WHERE matricule in (select matricule from loue)";
         PreparedStatement stat = conn.prepareStatement(sql);
         ResultSet rs = stat.executeQuery();
         
@@ -142,27 +170,46 @@ public class ClientController extends MainController {
 	
 	
 	@FXML private TextField input_matricule;
-	@FXML private TextField input_idProgramFidelite;
 	@FXML private TextField input_idUtilisateur;
 	@FXML private CheckBox input_assurance;
-	@FXML private CheckBox input_tauxReduction;
 	@FXML private TextField input_duree;
+	
+	@FXML private DatePicker input_date;
+	
+	@FXML private TextField input_idProgramFidelite;
+	@FXML private CheckBox input_tauxReduction;
+	
+	@FXML private ChoiceBox<Integer> input_etat = new ChoiceBox<Integer>();
+	@FXML private Label input_etat_label;
+	private Integer[] listOfEtat = {1,2,3,4,5};
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		input_etat.getItems().addAll(listOfEtat);
+		input_etat.setOnAction(this::updateEtatLabel);
+	}
+	
+	public void updateEtatLabel(ActionEvent event) {
+		String etat = input_etat.getValue().toString();
+		input_etat_label.setText(etat);
+	}
+	
 	@FXML private Label lbletat;
 	
 	
 	@FXML public void loue(ActionEvent event) {
-		PreparedStatement stat = null;
-		ResultSet rs = null;
-		String sql = "INSERT INTO loue (matricule, idUtilisateur, assurance, duree, datePriseDeReservation) VALUES (?, ?, ?, ?, ?)";
-
+		String sql = ("INSERT INTO loue (matricule, idUtilisateur, assurance, duree, datePriseDeReservation) VALUES (?, ?, ?, ?, ?)");
+		
 		try {
-			stat = conn.prepareStatement(sql);
-			stat.setString(1, input_matricule.getText().toString());
-			stat.setString(2, input_idUtilisateur.getText().toString());
-			stat.setString(3, input_assurance.getText().toString());
-			stat.setString(4, input_duree.getText().toString());
-			stat.setString(5, java.time.LocalDate.now().toString());
-			rs = stat.executeQuery();
+			PreparedStatement pst = conn.prepareStatement(sql);
+			
+			pst.setString(1, input_matricule.getText().toString());
+			pst.setInt(2, Integer.valueOf(input_idUtilisateur.getText().toString()));
+			pst.setBoolean(3, Boolean.valueOf(input_assurance.getText().toString()));
+			pst.setInt(4, Integer.valueOf(input_duree.getText().toString()));
+			pst.setString(5, java.time.LocalDate.now().toString());
+
+	        pst.executeUpdate();
 
 			lbletat.setText("Location enregistré");
 		} catch(Exception e) {
@@ -171,17 +218,18 @@ public class ClientController extends MainController {
 	}
 	
 	@FXML public void reserve(ActionEvent event) {
-		PreparedStatement stat = null;
-		ResultSet rs = null;
 		String sql = "INSERT INTO reserve (matricule, idUtilisateur, dateReservation, datePriseDeReservation) VALUES (?, ?, ?, ?)";
 
 		try {
-			stat = conn.prepareStatement(sql);
-			stat.setString(1, input_matricule.getText().toString());
-			stat.setString(2, input_idUtilisateur.getText().toString());
-			stat.setString(3, input_duree.getText().toString());
-			stat.setString(4, java.time.LocalDate.now().toString());
-			rs = stat.executeQuery();
+			PreparedStatement pst = conn.prepareStatement(sql);
+			
+			pst.setString(1, input_matricule.getText().toString());
+			pst.setInt(2, Integer.valueOf(input_idUtilisateur.getText().toString()));
+			String date = input_date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+			pst.setString(3, date.toString());
+			pst.setString(4, java.time.LocalDate.now().toString());
+			
+			pst.executeUpdate();
 
 			lbletat.setText("Réservation enregistré");
 		} catch(Exception e) {
@@ -190,21 +238,43 @@ public class ClientController extends MainController {
 	}
 	
 	@FXML public void retourne(ActionEvent event) {
-		
+		String sql = "INSERT INTO retourne (matricule, idUtilisateur, date, etatOrigine) VALUES (?, ?, ?, ?)";
+		String sql2 = "DELETE FROM loue WHERE matricule = ? AND idUtilisateur = ?";
+
+		try {
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, input_matricule.getText().toString());
+			pst.setInt(2, Integer.valueOf(input_idUtilisateur.getText().toString()));
+			pst.setString(3, java.time.LocalDate.now().toString());
+			pst.setInt(4, Integer.valueOf(input_etat_label.getText().toString()));
+			
+			
+			PreparedStatement pst2 = conn.prepareStatement(sql2);
+			pst2.setString(1, input_matricule.getText().toString());
+			pst2.setInt(2, Integer.valueOf(input_idUtilisateur.getText().toString()));
+			
+			
+			pst2.executeUpdate();
+			pst.executeUpdate();
+
+			lbletat.setText("Retour enregistré");
+		} catch(Exception e) {
+			lbletat.setText("Retour erreur");
+		}
 	}
 	
 	@FXML public void souscrit(ActionEvent event) {
-		PreparedStatement stat = null;
-		ResultSet rs = null;
-		String sql = "INSERT INTO reserve (idUtilisateur, idProgrammeFidelite, dateSouscription, tauxReduction) VALUES (?, ?, ?, ?)";
+		String sql = "INSERT INTO souscrire (idUtilisateur, idProgrammeFidelite, dateSouscription, tauxReduction) VALUES (?, ?, ?, ?)";
 
 		try {
-			stat = conn.prepareStatement(sql);
-			stat.setString(1, input_matricule.getText().toString());
-			stat.setString(2, input_idUtilisateur.getText().toString());
-			stat.setString(3, input_duree.getText().toString());
-			stat.setString(4, input_tauxReduction.getText().toString());
-			rs = stat.executeQuery();
+			PreparedStatement pst = conn.prepareStatement(sql);
+			
+			pst.setInt(1, Integer.valueOf(input_idUtilisateur.getText().toString()));
+			pst.setInt(2, Integer.valueOf(input_idProgramFidelite.getText().toString()));
+			pst.setString(3, java.time.LocalDate.now().toString());
+			pst.setBoolean(4, Boolean.valueOf(input_tauxReduction.getText().toString()));
+
+			pst.executeUpdate();
 
 			lbletat.setText("Souscription enregistré");
 		} catch(Exception e) {
@@ -252,6 +322,14 @@ public class ClientController extends MainController {
 	
 	public void goToClientFideliteArea(ActionEvent event) throws IOException, SQLException {
 		Parent root = FXMLLoader.load(getClass().getResource("/view/ClientFideliteArea.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void goToClientRetourArea(ActionEvent event) throws IOException, SQLException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/ClientRetourArea.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
